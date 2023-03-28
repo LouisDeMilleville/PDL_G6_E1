a5010f245c5fce2fa4bae62631fc878c6e3972b9
estelle <estelle.normand@groupe-esigelec.org>
2023-03-28 17:38:36 +0200

    modif class scolarite

--- PDL/src/Scolarite.java
+++ PDL/src/Scolarite.java
@@ -1,8 +1,17 @@
 
-public class Scolarite {
-
-	public Scolarite() {
-		// TODO Auto-generated constructor stub
+public class Scolarite extends Compte{
+	
+	private Boolean estGestionnaire;
+	
+	public Scolarite (int identifiant, String nom, String prenom, String mail, String mdp, Boolean estGestionnaire){
+		super(identifiant, nom, prenom, mail,mdp);
+		}
+	
+	public Boolean getGestionnaire() {
+		return true;
 	}
-
-}
+	
+	public void setGestionnaire() {
+		return true;
+	}
+}	
