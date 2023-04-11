d4edded99a4b908020eac3f4ee8d6682b77c4d26
23003925 <23003925@INFO-203-W01.intranet.int>
2023-04-11 09:38:11 +0200

    Test commit ignore

--- PDL/src/Main.java
+++ PDL/src/Main.java
@@ -2,7 +2,6 @@
 public class Main {
 
 	public static void main(String[] args) {
-		//Test commit
 		// TODO Auto-generated method stub
 
 		LoginWindow fenetreConnexion = new LoginWindow("EsigServices", 800, 400);
