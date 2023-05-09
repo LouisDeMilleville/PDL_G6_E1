9940075a3dc9d89d97717b7a3e4851925e4fe39d
LouisDeMilleville <125300400+LouisDeMilleville@users.noreply.github.com>
2023-05-09 09:40:56 +0200

    Ajout de la javadoc pour Cours, DateEtHeure et Enseignant

--- PDL/src/Cours.java
+++ PDL/src/Cours.java
@@ -1,3 +1,11 @@
+/**
+ * Cette classe représente un cours
+ * @author Equipe 1 groupe 6
+ * 
+ * @version 1.0
+ *
+ */
+
 public class Cours {
     private String cou_id;
 	private int num_cours;
@@ -29,94 +37,195 @@ public class Cours {
 		this.estAbsent=estAbsent;			
 	}
 	
+	/**
+	 * Renvoie l'id du cours dans la bdd
+	 * @return id du cours
+	 */
 	public String getIdCours() {
 	    	return cou_id;
 	}
-				
+	
+	/**
+	 * Renvoie le nombre de fois où ce cours est dispensé durant la semaine
+	 * @return nombre de cours de la semaine
+	 */
 	public int getNumCours() {
 		return num_cours;
 	}
     	
+	/**
+	 * Renvoie la matiere du cours
+	 * @return matiere du cours
+	 */
 	public String getMatiere() {
 		return matiere;
 	}
 	
+	/**
+	 * Renvoie la duree du cours (en minutes
+	 * @return duree du cours
+	 */
 	public int getDuree() {
 		return duree;
 	}
 	
+	/**
+	 * Renvoie la date et l'heure du debut du cours (annee, mois, jour, heure, minute)
+	 * @return Date et heure du debut du cours
+	 */
 	public DateEtHeure getDateEtHeure() {
 		return date_heure;
 	}
    
+	/**
+	 * Renvoie la salle du cours
+	 * @return salle du cours
+	 */
 	public String getSalle() {
 		return salle;
 	}
 	
+	/**
+	 * Renvoie le type du cours
+	 * 0 = CM
+	 * 1 = TD
+	 * 2 = TP
+	 * 3 = DS
+	 * @return type du cours
+	 */
 	public int getTypeCours() {
 		return type_de_cours;
 	}
 	
+	/**
+	 * Renvoie l'id de l'enseignant initial du cours
+	 * @return id de l'enseignant initial
+	 */
 	public int getEns() {
 		return id_ens;
 	}
 	
+	/**
+	 * Renvoie l'id du groupe concerne
+	 * @return id du groupe
+	 */
 	public int getGrp() {
 		return id_grp;
 	}
 	
+	/**
+	 * Renvoie l'id de l'enseignant remplacant
+	 * @return id de l'enseignant remplacant
+	 */
 	public int getRemp() {
 		return id_ens_remp;
 	}
 	
+	/**
+	 * Renvoie l'etat d'absence de l'enseignant initial
+	 * 0 = present
+	 * 1 = absent
+	 * @return etat d'absence enseignant initial
+	 */
 	public int getAbsent() {
 		return estAbsent;
 	}
 	
+	/**
+	 * Affecte un nouveau nombre de cours pour la semaine
+	 * @param nombre de cours semaine
+	 */
 	public void setNumCours(int numcours) {
 		this.num_cours=numcours;
 	}
 	
+	/**
+	 * Affecte une nouvelle duree au cours
+	 * @param nouvelle duree du cours (en minutes)
+	 */
 	public void setDuree(int duree) {
 		this.duree=duree;
 	}
 	
+	/**
+	 * Affecte une nouvelle date et heure de debut de cours
+	 * @param nouvelle date et heure de debut de cours
+	 */
 	public void setDateEtHeure(DateEtHeure dateEtHeure) {
 		this.date_heure=dateEtHeure;
 	}
 	
+	/**
+	 * Affecte une nouvelle salle au cours
+	 * @param Nouvelle salle du cours
+	 */
 	public void setSalle(String salle) {
 		this.salle=salle;
 	}
 	
+	/**
+	 * Affecte une nouvelle matiere au cours
+	 * @param nouvelle matiere du cours
+	 */
 	public void setMatiere(String matiere) {
 			this.matiere=matiere;
 	}
 	
+	/**
+	 * Affecte un nouvel id au cours
+	 * @param nouvel id du cours
+	 */
 	public void setIdCours(String idCours) {
 			this.cou_id=idCours;
 	}
 	
+	/**
+	 * Affecte un nouveau remplacant au cours
+	 * @param id du nouvel enseignant remplacant
+	 */
 	public void setRemp(int id_remp) {
 		this.id_ens_remp=id_remp;
 	}
 	
+	/**
+	 * Affecte un nouveau type de cours 
+	 * 0 = CM
+	 * 1 = TD
+	 * 2 = TP
+	 * 3 = DS
+	 * @param nouveau type du cours
+	 */
 	public void setTypeCours(int typeCours) {
 			this.type_de_cours=typeCours;
 	}
 	
+	/**
+	 * Affecte un nouvel id de groupe
+	 * @param id nouveau groupe
+	 */
 	public void setIdGroupe(int idGroupe) {
 		this.id_grp=idGroupe;
 	}
 	
+	/**
+	 * Affecte un nouvel enseignant initial
+	 * @param id nouvel enseignant initial
+	 */
 	public void setEns(int id_ens) {
 		this.id_ens=id_ens;
 	}
 	
+	/**
+	 * Change l'etat d'absence de l'enseignant initial
+	 * @param nouvel etat absence enseignant initial
+	 */
 	public void setAbsent(int absent) {
 		this.estAbsent=absent;
 	}
 	
+	/**
+	 * Affiche les infos du cours dans la console
+	 */
 	public void display() {
 		System.out.println("id du Cours: "+cou_id);
 		System.out.println("num du Cours: "+num_cours);
