45569d3365ad17c5471d5213c5c6e5a186ce2bfd
allagbeolushade <allagbeolushade@localhost>
2023-05-16 10:14:06 +0200

    newCommit

--- PDL/src/pdl/model/ScolariteModel.java
+++ PDL/src/pdl/model/ScolariteModel.java
@@ -1,3 +1,13 @@
+/**
+ * @author Shadé
+ * @version 1.0
+ * @param identifiant l'identifiant du gestionnaire
+ * @param estGestionnaire notifie que la personne est gestionnaire
+ * @param nom le nom du gestionnaire
+ * @param prenom le prénom du gestionnaie
+ * @param mail l'adresse mail du gestionnaire 
+ * @param mdp mot de passe du gestionnaire
+ */
 package pdl.model;
 
 public class ScolariteModel  extends CompteModel{
@@ -10,10 +20,18 @@ public class ScolariteModel  extends CompteModel{
 
 		this.estGestionnaire = estGestionnaire;
 	}
-	
+	/**
+	 * 
+	 * @return estGestionnaire
+	 */
 	public boolean getGestionnaire() {
 		return estGestionnaire;
 	}
+	/**
+	 * 
+	 * @param estGest le gestionnaire modifié
+	 * @return estGest
+	 */
 	public void setGestionnaire(boolean estGest) {
 		this.estGestionnaire = estGest;
 	}
