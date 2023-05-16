b7ce172ff059165d416a126a484e029408be8e64
allagbeolushade <allagbeolushade@localhost>
2023-05-16 10:45:18 +0200

    newnew

--- PDL/src/pdl/model/AbsenceDistancielModel.java
+++ PDL/src/pdl/model/AbsenceDistancielModel.java
@@ -1,3 +1,13 @@
+/**
+ * @author allagbeolushade
+ * @version 1.0
+ * @param id l'identifiant de l'utilisateur
+ * @param etatJustificatif justifie le caractère envoyé du justificatif
+ * @param matiere la matiere pour laquelle l'on voudrait s'absenter 
+ * @param duree la durée de la matière
+ * @param nbCours le nombre de cours que l'on devrait rater si l'on est absent
+ * @param link le lien d'accès au cours en distanciel
+ */
 package pdl.model;
 
 public class AbsenceDistancielModel extends JustificatifModel {
@@ -13,7 +23,21 @@ private int id;
 	private int nbCours;
 	
 	private String link;
-	
+	/**
+	 * le constructeur
+	 * @param id
+	 * @param etatJustificatif
+	 * @param matiere
+	 * @param duree
+	 * @param nbCours
+	 * @param typeJustif
+	 * @param link
+	 * @param annee
+	 * @param mois
+	 * @param jour
+	 * @param heure
+	 * @param minute
+	 */
 	public AbsenceDistancielModel(int id, int etatJustificatif, String matiere, int duree, int nbCours, String typeJustif, String link, int annee, int mois, int jour, int heure, int minute) {
 		super(typeJustif, annee, mois, jour, heure, minute);
 		// TODO Auto-generated constructor stub
@@ -26,33 +50,66 @@ private int id;
 		this.link = link;
 			
 	}
-	
+	/**
+	 * 
+	 * @param lien le lien modifié
+	 * @erturn lien 
+	 * 
+	 */
 	public void setLink(String lien) {
 		this.link = lien;
 	
 	}
+	/**
+	 * 
+	 * @return nbCours
+	 */
 
 	public int getNbCours() {
 		return nbCours;
 	}
-
+    /**
+     * 
+     * @return duree
+     */
 	public int getDuree() {
 		return duree;
 	}
+	/**
+	 * 
+	 * @return matiere
+	 */
 
 	public String getMatiere() {
 		return matiere;
 	}
+	/**
+	 * 
+	 * @return etatJustificatif
+	 */
 
 	public int getEtatJustificatif() {
 		return etatJustificatif;
 	}
+	/**
+	 * 
+	 * @return id
+	 */
 
 	public int getId() {
 		return id;
 	}
+	/**
+	 * 
+	 * @param etat l'état du justificatif modifié
+	 * @return etat
+	 * 
+	 */
 	
 	public void setEtatJustificatif(int etat) {
 		this.etatJustificatif = etat;
 	}
 }
+
+	
+
--- PDL/src/pdl/model/DateEtHeureModel.java
+++ PDL/src/pdl/model/DateEtHeureModel.java
@@ -1,3 +1,12 @@
+/**
+ * @author allagbeolushade
+ * @version 1.0
+ * @param annee  l'année 
+ * @param mois le mois 
+ * @param jour le jour
+ * @param heure l'heure
+ * @param minute la minute
+ */
 package pdl.model;
 
 public class DateEtHeureModel {
@@ -11,7 +20,14 @@ private int annee;
 	private int heure;
 	
 	private int minute;
-	
+	/**
+	 * le constructeur date et heure de l'application utilisable en extension
+	 * @param annee
+	 * @param mois
+	 * @param jour
+	 * @param heure
+	 * @param minute
+	 */
 	public DateEtHeureModel (int annee, int mois, int jour , int heure, int minute) {
 		
 		this.annee = annee;
@@ -21,43 +37,78 @@ private int annee;
 		this.minute = minute;
 	}
 	
-
+/**
+ * 
+ * @return annee
+ */
 	public int getAnnee() {
 		return annee;
 	}
-	
+	/**
+	 * 
+	 * @return mois
+	 */
 	public int getMois() {
 		return mois;
 	}
-	
+	/**
+	 * 
+	 * @return jour
+	 */
 	public int getJour() {
 		return jour;
 	}
-	
+	/**
+	 * 
+	 * @return heure
+	 */
 	public int getHeure() {
 		return heure;
 	}
-	
+	/**
+	 * 
+	 * @return minute
+	 */
 	public int getMinute() {
 		return minute;
 	}
-	
+	/**
+	 * 
+	 * @param annee l'année modifiée
+	 * @return annee
+	 */
 	public void setAnnee(int annee) {
 		this.annee = annee;
 	}
-	
+	/**
+	 * 
+	 * @param mois le mois modifié
+	 * @return mois
+	 */
 	public void setMois(int mois) {
 		this.mois = mois;
 	}
-	
+	/**
+	 * 
+	 * @param jour le jour modifié
+	 * @return jour
+	 */
 	public void setJour(int jour) {
 		this.jour = jour;
 	}
-	
+	/**
+	 * 
+	 * @param heure l'heure modifiée
+	 * @return heure
+	 */
 	public void setHeure(int heure) {
 		this.heure = heure;
 	}
-	
+	/**
+	 * 
+	 * @param minute la minute modifée
+	 * @return minute
+	 */
 	public void setMinute(int minute) {
 		this.minute = minute;
 	}
